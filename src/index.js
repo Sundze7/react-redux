927df604efacacd69595fc161824fc61b1542897
Sundze <keyonmwesundze@gmail.com>
2025-06-15 15:46:23 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -4,24 +4,43 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 
+const firstBook = {
+  author: "Bill O'neill",
+  title: "Random Fun Facts",
+  img: "./images/book1.jpg",
+};
+
+const secondBook = {
+  author: "Nat Geo",
+  title: "50 States 5k Ideas",
+  img: "./images/book2.jpg",
+};
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
       <Book />
       <Book />
     </section>
   );
 };
 
-const Book = () => {
-  const author = "Bill O'neill";
-  const title = "Random Fun Facts";
+const Book = (props) => {
   return (
     <article className="book">
-      <img src="./images/book1.jpg" alt="Random Fun Facts" />;<h2>{title}</h2>
-      <h4>{author.toUpperCase()}</h4>
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };
