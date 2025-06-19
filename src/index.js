9db6569c0734f9e3dc6eb19b3cfb86bd18e49201
Sundze <keyonmwesundze@gmail.com>
2025-06-19 15:42:25 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ const books = [
 const BookList = () => {
   return (
     <section className="booklist">
+      <EventExample />
       {books.map((book) => {
         // const { img, title, author, id } = book;
         // return <Book key={id} img={img} title={title} author={author} />; OR
@@ -31,6 +32,30 @@ const BookList = () => {
 
         return <Book {...book} key={book.id} />;
       })}
+    </section>
+  );
+};
+
+const EventExample = () => {
+  const handleFormInput = (e) => {
+    console.log(e.target.value);
+    console.log("handle form input");
+  };
+  const handleButtonClick = () => {
+    alert("handle button click");
+  };
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: "1rem 0" }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
     </section>
   );
 };
