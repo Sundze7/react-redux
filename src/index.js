17d3a7e6bb1e357417db988a60d2ea9e7d5eb73b
Sundze <keyonmwesundze@gmail.com>
2025-06-24 16:43:16 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -21,51 +21,62 @@ const books = [
 ];
 
 const BookList = () => {
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
   return (
     <section className="booklist">
-      <EventExample />
+      {/* <EventExample /> */}
       {books.map((book) => {
         // const { img, title, author, id } = book;
         // return <Book key={id} img={img} title={title} author={author} />; OR
 
         // return <Book book={book} key={book.id} />; OR
 
-        return <Book {...book} key={book.id} />;
+        return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
   );
 };
 
-const EventExample = () => {
-  const handleFormInput = (e) => {
-    console.log(e.target.value);
-    console.log("handle form input");
-  };
-  const handleButtonClick = () => {
-    alert("handle button click");
-  };
-  return (
-    <section>
-      <form>
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="example"
-          onChange={handleFormInput}
-          style={{ margin: "1rem 0" }}
-        />
-      </form>
-      <button onClick={handleButtonClick}>click me</button>
-    </section>
-  );
-};
+// const EventExample = () => {
+//   const handleFormInput = (e) => {
+//     console.log(e.target.value);
+//     console.log("handle form input");
+//   };
+//   const handleButtonClick = () => {
+//     alert("handle button click");
+//   };
+//   return (
+//     <section>
+//       <form>
+//         <h2>Typical Form</h2>
+//         <input
+//           type="text"
+//           name="example"
+//           onChange={handleFormInput}
+//           style={{ margin: "1rem 0" }}
+//         />
+//       </form>
+//       <button onClick={handleButtonClick}>click me</button>
+//     </section>
+//   );
+// };
 
 const Book = (props) => {
-  const { img, title, author } = props;
+  const { img, title, author, getBook, id } = props;
+  // this is a wrapper function which helps pass down the id of getBook() as prop from parent to child
+  const getSingleBook = () => {
+    getBook(id);
+  };
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={getSingleBook} style={{ padding: "0.5rem" }}>
+        Click me
+      </button>
       <h4>{author}</h4>
       {/* <p>
         For some reason i just cant get lorem to generate text for me in this
